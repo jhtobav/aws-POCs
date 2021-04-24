31d96be0397f68ce34a06fcb44dcb1902c13ff13
Jaime Toba <jhtobav@gmail.com>
2021-04-24 14:37:02 -0500

    nuevos proyectos

---
+++ cognito-passwordless-auth-mongodb/client/src/aws-exports.js
@@ -0,0 +1,7 @@
+const awsmobile = {
+"aws_project_region": "us-east-1",
+"aws_cognito_region": "us-east-1",
+"aws_user_pools_id": "us-east-1_HHZ3KrfPj",
+"aws_user_pools_web_client_id": "7esoq4b1cu9l5uuhbgso5gn2a9",
+};
+export default awsmobile;
